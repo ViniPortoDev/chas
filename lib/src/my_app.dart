9f7731b1c952c845e0d6d5741656c5df6bffdd97
Vini Porto <vinicius.p.a.corin@gmail.com>
2023-11-20 20:15:51 -0300

    finalizando design do projeto e tela de info mock

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -6,9 +6,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeView(),
+      theme: ThemeData(fontFamily: 'Nunito'),
+      home: const HomeView(),
     );
   }
 }
